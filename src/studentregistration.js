a523902a750a20b773af59a48615b057ac8bacc7
prudvi raju <prudvi.kallepalli@gmail.com>
2019-11-15 15:51:30 +0530

    Revert "Check box Component"
    This reverts commit ddbdd67bcedc17566195ed7aa725061c846c1750.

--- src/studentregistration.js
+++ src/studentregistration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, NumberField, EmailField, FormButton, RadioGroup, SelectField, CheckBox } from './formcomponent';
+import { TextField, NumberField, EmailField, FormButton, RadioGroup, SelectField } from './formcomponent';
 import './studentregistration.css';
 import { states_in_india } from './listofstates';
 
@@ -12,8 +12,6 @@ const StudentRegistrationForm = () => {
                 <NumberField fieldname="phonenumber" numberlength="10" placeholder="enter phonenumber" />
                 <EmailField fieldname="email" placeholder="enter  emailid" />
                 <RadioGroup fieldname="gender" labels="male female other" />
-                <CheckBox fieldname="firstlanguage" fieldvalue="telugu" label="Telugu" />
-                <CheckBox fieldname="secondlanguage" fieldvalue="english" label="English" />
                 <SelectField label="choose state:" fieldname="personstate" options={states_in_india} />
                 <FormButton shouldfill="firstname&&lastname&&phonenumber&&email&&gender&&personstate" label="submit" />
             </form>
